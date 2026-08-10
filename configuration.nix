2e2fe5bf25e6f824c1b08fb4c9a2739ad628dcd8
slfhrmfn <i@jumoroz.ru>
2026-08-11 00:01:29 +0300

    Update

--- configuration.nix
+++ configuration.nix
@@ -9,15 +9,13 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-#moe2
-#moe3
-environment.shellAliases = {
-  nix-upgrade = "cd /etc/nixos && sudo nix flake update; sudo git add .; sudo git commit -m 'Update' || true; sudo nixos-rebuild switch --flake .#nix-btw";
-};
 
+  environment.shellAliases = {
+    nix-upgrade = "cd /etc/nixos && sudo nix flake update; sudo git add .; sudo git commit -m 'Update' || true; sudo nixos-rebuild switch --flake .#nix-btw";
+  };
 
-nix.settings.experimental-features = [ "nix-command" "flakes" ];
-#moe
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   nix.settings = {
     # Скачивать пакеты параллельно
     max-jobs = "auto";
@@ -32,11 +30,12 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
     trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
   };
 
-# Bootloader.
-    boot.loader.timeout = 5; # Меню будет отображаться 5 секунд
+  # Bootloader.
+  boot.loader.timeout = 5; # Меню будет отображаться 5 секунд
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-#font 
+
+  # Fonts
   fonts.packages = with pkgs; [
     corefonts # Этот пакет содержит Comic Sans, Arial, Times New Roman и др.
     nerd-fonts.jetbrains-mono
@@ -46,11 +45,6 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nix-btw"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -77,16 +71,14 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
+  services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
 
-services.desktopManager.gnome.enable = true;
-services.displayManager.gdm.enable = true;
   # Configure keymap in X11
   services.xserver.xkb = {
-      layout = "us,ru"; # Ваши раскладки
+    layout = "us,ru"; # Ваши раскладки
     options = "grp:caps_toggle"; # Переключение по Caps Lock
-
   };
-  
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -99,16 +91,7 @@ services.displayManager.gdm.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."slfhrmfn" = {
@@ -116,12 +99,8 @@ services.displayManager.gdm.enable = true;
     description = "slfhrmfn";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    neovim
-    fastfetch
-    curl
-    wget
-    ghostty
-    git
+      ghostty
+      vlc
     ];
   };
 
@@ -131,42 +110,14 @@ services.displayManager.gdm.enable = true;
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # List packages installed in system profile.
   environment.systemPackages = with pkgs; [
-  neovim
-  fastfetch
-  curl
-  wget
-  ghostty
-  git
+    neovim
+    fastfetch
+    curl
+    wget
+    git
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "26.05"; # Did you read the comment?
-
+  system.stateVersion = "26.05";
 }
