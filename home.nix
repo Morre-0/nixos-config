7ba89b8c20dd4ebf58f408663b6bd04239ba472f
slfhrmfn <i@jumoroz.ru>
2026-08-11 01:07:41 +0300

    Update

--- home.nix
+++ home.nix
@@ -1,4 +1,17 @@
 { pkgs, ... }: {
+  # Автоматически скачиваем и устанавливаем NvChad с GitHub
+  home.file.".config/nvim" = {
+    source = pkgs.fetchFromGitHub {
+      owner = "Morre-0";
+      repo = "nvchad-config";
+      # Точный хэш коммита (для воспроизводимости Flakes)
+      rev = "master"; 
+      # Специальный хэш безопасности (Nix проверит целостность скачанного)
+      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    };
+    # Разрешаем обновлять файлы, если репозиторий изменится
+    recursive = true;
+  };
   # Версия Home Manager (оставляем ту, что была)
   home.stateVersion = "26.05"; 
 
