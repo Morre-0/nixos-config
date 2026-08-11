20fed1f6ab6f70f47d5ee52fee323e0e458ed580
slfhrmfn <i@jumoroz.ru>
2026-08-11 22:41:02 +0300

    Update

--- modules/core/nix-settings.nix
+++ modules/core/nix-settings.nix
@@ -3,12 +3,14 @@
 
   environment.systemPackages = with pkgs; [
     neovim
+    fastfetch
     git
     curl
     wget
   ];
 
   environment.shellAliases = {
+      fetch = "fastfetch"; # Короткая команда для запуска fastfetch
     nix-clean = "nix-env --delete-generations old && sudo nix-env -p /nix/var/nix/profiles/system --delete-generations old && sudo nix-collect-garbage -d && sudo nix-store --optimize && sudo nixos-rebuild boot --flake /etc/nixos/#nix-btw";
   };
 }
