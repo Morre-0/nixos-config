fdaf42a815f780b7004fa1e09322aa3a93281a19
slfhrmfn <i@jumoroz.ru>
2026-08-11 16:20:36 +0300

    Update

--- flake.nix
+++ flake.nix
@@ -1,15 +1,20 @@
 {
-  description = "Конфигурация NixOS на Flakes с интеграцией Home Manager";
+  description = "Конфигурация NixOS на Flakes с интеграцией Home Manager и фиксацией Hyprland v0.52.0";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # ИСПРАВЛЕНО: Вынесли Hyprland v0.52.0 на правильный уровень инпутов
+    hyprland.url = "github:hyprwm/Hyprland/v0.52.0";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  # ИСПРАВЛЕНО: Явно передали hyprland в аргументы outputs
+  outputs = { self, nixpkgs, home-manager, hyprland, ... }@inputs: {
     nixosConfigurations = {
       nix-btw = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -20,9 +25,14 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            
-            # Вместо длинного кода просто импортируем наш новый файл:
             home-manager.users.slfhrmfn = import ./home.nix;
+
+            # ИСПРАВЛЕНО: Добавили оверлей, который принудительно заменяет версию Hyprland во всей системе
+            nixpkgs.overlays = [
+              (final: prev: {
+                hyprland = hyprland.packages.${prev.system}.hyprland;
+              })
+            ];
           }
         ];
       };
