d955bd1776ff1b5b014ba9b6943fea780c56baf9
slfhrmfn <i@jumoroz.ru>
2026-08-11 23:08:08 +0300

    chore: авто-коммит при обновлении системы

---
+++ modules/desktop/hyprland-apps.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    # Интерфейс
+    waybar              # Твой статус-бар
+    rofi        # Меню запуска приложений
+    dunst               # Сервер уведомлений
+    swww                # Менеджер обоев
+
+    # Утилиты экрана и буфера
+    grim                # Скриншоты
+    slurp               # Выбор области для скриншота
+    wl-clipboard        # Буфер обмена
+    cliphist            # История буфера обмена
+
+    # Системное
+    xfce.thunar         # Файловый менеджер
+    lxappearance        # Настройка GTK тем
+  ];
+
+  # Включаем важный портал для правильной работы скриншотов в Hyprland
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+  };
+}
+
