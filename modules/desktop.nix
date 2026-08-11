2f752d7a096e29d019914d309010978b582a3580
slfhrmfn <i@jumoroz.ru>
2026-08-11 22:33:21 +0300

    Update

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -2,10 +2,16 @@
   # Включаем Hyprland на системном уровне
   programs.hyprland.enable = true;
 
-  # Автоматический вход в систему без экранных менеджеров
+  # Настраиваем по-настоящему автоматический вход в систему
   services.greetd = {
     enable = true;
     settings = {
+      # 1. Первая сессия при загрузке — заходим сразу без пароля
+      initial_session = {
+        command = "Hyprland";
+        user = "slfhrmfn";
+      };
+      # 2. Если ты случайно выйдешь из Hyprland, тебя вернет в текстовый вход
       default_session = {
         command = "${pkgs.greetd}/bin/agreety --cmd Hyprland";
         user = "slfhrmfn";
