19600e48ca921d8816f928240627a6a591893ccc
slfhrmfn <i@jumoroz.ru>
2026-08-11 16:28:33 +0300

    Update

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Конфигурация NixOS на Flakes с фиксацией стабильного Hyprland v0.52.0 через точный коммит Nixpkgs";
+  description = "Конфигурация NixOS на Flakes с фиксацией стабильного Hyprland без Lua из ветки 24.11";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
@@ -9,14 +9,15 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # ИСПРАВЛЕНО: Реальный, валидный коммит Nixpkgs, содержащий Hyprland версии 0.52.0
-    nixpkgs-hyprland.url = "github:nixos/nixpkgs/e6e06dd95e38148b598b9daed2e6ba7ddb8d6fbb";
+    # ИСПРАВЛЕНО: Подключаем официальную стабильную ветку, где Hyprland работает строго на классических .conf файлах
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs, home-manager, nixpkgs-hyprland, ... }@inputs: 
+  outputs = { self, nixpkgs, home-manager, nixpkgs-stable, ... }@inputs: 
   let
     system = "x86_64-linux";
-    pkgs-old = import nixpkgs-hyprland { inherit system; config.allowUnfree = true; };
+    # Инициализируем стабильные пакеты без Lua
+    pkgs-stable = import nixpkgs-stable { inherit system; config.allowUnfree = true; };
   in {
     nixosConfigurations = {
       nix-btw = nixpkgs.lib.nixosSystem {
@@ -30,11 +31,12 @@
             home-manager.useUserPackages = true;
             home-manager.users.slfhrmfn = import ./home.nix;
 
-            # Подменяем бинарник и сессии GDM на версию 0.52.0
+            # ИСПРАВЛЕНО: Этот оверлей подменяет бинарник и порталы сессии на версию из ветки 24.11.
+            # Она никогда не выдаст 404, а GDM сразу увидит .desktop файл запуска.
             nixpkgs.overlays = [
               (final: prev: {
-                hyprland = pkgs-old.hyprland;
-                xdg-desktop-portal-hyprland = pkgs-old.xdg-desktop-portal-hyprland;
+                hyprland = pkgs-stable.hyprland;
+                xdg-desktop-portal-hyprland = pkgs-stable.xdg-desktop-portal-hyprland;
               })
             ];
           }
