056578466a8d9a3e15f1ab73326f7613af959609
slfhrmfn <i@jumoroz.ru>
2026-08-11 21:56:39 +0300

    docs: настроил точную геометрию disko под nvme1n1

--- disko-config.nix
+++ disko-config.nix
@@ -1,16 +1,18 @@
 {
   disko.devices = {
     disk = {
+      # Настраиваем конкретно диск nvme1n1
       main = {
         type = "disk";
-        device = "/dev/nvme0n1";
+        device = "/dev/nvme1n1";
         content = {
           type = "gpt";
           partitions = {
+            # 1. Открытый загрузочный раздел EFI (1 Гигабайт, как в lsblk)
             ESP = {
               priority = 1;
               name = "ESP";
-              size = "512M";
+              size = "1G";
               type = "EF00";
               content = {
                 type = "filesystem";
@@ -19,8 +21,9 @@
                 mountOptions = [ "umask=0077" ];
               };
             };
+            # 2. Зашифрованный LUKS-контейнер для Btrfs (467.1 Гигабайт)
             luks = {
-              size = "100%";
+              size = "467.1G";
               content = {
                 type = "luks";
                 name = "crypted";
@@ -29,18 +32,17 @@
                   type = "btrfs";
                   extraArgs = [ "-f" ];
                   subvolumes = {
+                    # Корень системы (/)
                     "@root" = {
                       mountpoint = "/";
                       mountOptions = [ "compress=zstd" "noatime" ];
                     };
+                    # Домашние папки (/home)
                     "@home" = {
                       mountpoint = "/home";
                       mountOptions = [ "compress=zstd" "noatime" ];
                     };
-                    "@var" = {
-                      mountpoint = "/var";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
+                    # Хранилище пакетов (/nix)
                     "@nix" = {
                       mountpoint = "/nix";
                       mountOptions = [ "compress=zstd" "noatime" ];
@@ -49,9 +51,18 @@
                 };
               };
             };
+            # 3. Раздел подкачки Swap (8.8 Гигабайт, как в lsblk)
+            swap = {
+              size = "8.8G";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+              };
+            };
           };
         };
       };
     };
   };
 }
+
