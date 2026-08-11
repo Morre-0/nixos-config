2b7d63a16079b24ea47fee86d9051e2c2cf8665c
Morre-0 <slfhrmfn@example.com>
2026-08-12 00:43:21 +0300

    chore: авто-коммит при обновлении системы

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,6 +1,10 @@
-{ ... }: {
+{ pkgs, ... }: {
+  # Загрузчик systemd-boot
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.supportedFilesystems = [ "btrfs" ];
+
+  # ИСПРАВЛЕНО: Принудительно переключаем систему на последнее mainline-ядро Linux
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
 
