7ff14886333a4b27b6e86c629dd370b395564a5f
Morre-0 <slfhrmfn@example.com>
2026-08-12 15:56:01 +0300

    chore: авто-коммит при обновлении системы

--- user/home.nix
+++ user/home.nix
@@ -1,58 +1,61 @@
 inputs: { pkgs, ... }: {
   home.stateVersion = "26.05"; 
 
-  # Пакеты пользователя (чистый GNOME Workstation)
   home.packages = with pkgs; [
-    btop
-    htop
-    cmus
-    vlc
-    steam-run
-    gcc
-    luarocks
-    pywalfox-native
-    
-    # Твой браузер Zen Browser из внешнего флейка
+    btop htop cmus vlc steam-run gcc luarocks pywalfox-native
     inputs.zen-browser.packages."${pkgs.system}".default
-
-    # Изолированное окружение Python
-    (python3.withPackages (ps: with ps; [
-      pip
-      virtualenv
-      setuptools
-    ]))
+    inputs.picom-pijulius.packages."${pkgs.system}".default
+    (python3.withPackages (ps: with ps; [ pip virtualenv setuptools ]))
   ];
 
-  # Настройка Firefox с поддержкой Pywalfox
   programs.firefox = {
     enable = true;
-    nativeMessagingHosts = with pkgs; [
-      pywalfox-native
-    ];
+    nativeMessagingHosts = with pkgs; [ pywalfox-native ];
   };
 
-  # Декларативная настройка встроенного Vim
-  programs.vim = {
+  # УЛЬТИМАТИВНАЯ ДЕКЛАРАТИВНАЯ НАСТРОЙКА NEOVIM (GRUVBOX MATERIAL)
+  programs.neovim = {
     enable = true;
-    settings = {
-      number = true;
-      relativenumber = true;
-      tabstop = 4;
-      shiftwidth = 4;
-      expandtab = true;
-    };
-    extraConfig = ''
-      syntax on
-      set mouse=a
-      set clipboard=unnamedplus
+    defaultEditor = true; # Делаем Neovim стандартным редактором в системе
+    viAlias = true;
+    vimAlias = true;
+
+    # Устанавливаем тему напрямую через Nix пакетом
+    plugins = with pkgs.vimPlugins; [
+      gruvbox-material
+    ];
+
+    # Конфигурация редактора на чистом Lua
+    extraLuaConfig = ''
+      -- 1. Системные настройки интерфейса
+      vim.opt.number = true           -- Включаем нумерацию строк
+      vim.opt.relativenumber = true   -- Включаем относительные номера строк (удобно для навигации)
+      vim.opt.termguicolors = true    -- Включаем 24-битные TrueColor цвета (критично для Gruvbox)
+      vim.opt.tabstop = 4             -- Размер табуляции (4 пробела)
+      vim.opt.shiftwidth = 4
+      vim.opt.expandtab = true        -- Превращать табы в пробелы
+      vim.opt.mouse = "a"             -- Включаем поддержку мыши
+      vim.clipboard = "unnamedplus"   -- Общий буфер обмена с системой (X11/xclip)
+
+      -- 2. Настройка палитры Gruvbox Material
+      vim.g.gruvbox_material_background = "medium" -- Варианты: 'hard', 'medium', 'soft'
+      vim.g.gruvbox_material_better_performance = 1
+
+      -- 3. Активируем тему оформления
+      vim.cmd("colorscheme gruvbox-material")
     '';
   };
 
-  programs.home-manager.enable = true;
+  # Твои папки автоматического развертывания
   home.file = {
+    "Pictures/Wallpapers" = { source = ./dotfiles/wallpapers; recursive = true; };
     ".config/scripts" = { source = ./dotfiles/scripts; recursive = true; executable = true; };
     ".config/alacritty" = { source = ./dotfiles/alacritty; recursive = true; };
     ".config/polybar" = { source = ./dotfiles/polybar; recursive = true; };
+    ".config/sxhkd" = { source = ./dotfiles/sxhkd; recursive = true; };
+    ".config/bspwm" = { source = ./dotfiles/bspwm; recursive = true; executable = true; };
   };
 
+  programs.home-manager.enable = true;
 }
+
