db822b1daf4ba160011a09d89dfe06886085125d
Morre-0 <slfhrmfn@example.com>
2026-08-12 15:58:36 +0300

    chore: авто-коммит при обновлении системы

--- user/home.nix
+++ user/home.nix
@@ -13,16 +13,21 @@ inputs: { pkgs, ... }: {
     nativeMessagingHosts = with pkgs; [ pywalfox-native ];
   };
 
-  # УЛЬТИМАТИВНАЯ ДЕКЛАРАТИВНАЯ НАСТРОЙКА NEOVIM (GRUVBOX MATERIAL)
+  # УЛЬТИМАТИВНАЯ ДЕКЛАРАТИВНАЯ НАСТРОЙКА NEOVIM (GRUVBOX MATERIAL С ПЛАГИНАМИ)
   programs.neovim = {
     enable = true;
     defaultEditor = true; # Делаем Neovim стандартным редактором в системе
     viAlias = true;
     vimAlias = true;
 
-    # Устанавливаем тему напрямую через Nix пакетом
+    # Устанавливаем тему и плагины напрямую через Nix пакеты
     plugins = with pkgs.vimPlugins; [
-      gruvbox-material
+      gruvbox-material         # Наша основная тема
+      lualine-nvim             # Красивая статус-строка внизу
+      nvim-web-devicons        # Иконки для статус-строки и плагинов
+      (nvim-treesitter.withPlugins (p: [ 
+        p.nix p.lua p.bash p.python p.json p.markdown p.yaml
+      ]))                      # Мощная TrueColor подсветка синтаксиса
     ];
 
     # Конфигурация редактора на чистом Lua
@@ -35,14 +40,36 @@ inputs: { pkgs, ... }: {
       vim.opt.shiftwidth = 4
       vim.opt.expandtab = true        -- Превращать табы в пробелы
       vim.opt.mouse = "a"             -- Включаем поддержку мыши
-      vim.clipboard = "unnamedplus"   -- Общий буфер обмена с системой (X11/xclip)
+      vim.opt.clipboard = "unnamedplus" -- Общий буфер обмена с системой (X11/xclip)
+      vim.opt.smartindent = true      -- Умные автоотступы при вводе кода
+      vim.opt.wrap = false            -- Не переносить длинные строки автоматически
 
       -- 2. Настройка палитры Gruvbox Material
       vim.g.gruvbox_material_background = "medium" -- Варианты: 'hard', 'medium', 'soft'
       vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_enable_bold = 1
 
       -- 3. Активируем тему оформления
       vim.cmd("colorscheme gruvbox-material")
+
+      -- 4. Настройка статус-строки Lualine под тему Gruvbox
+      require('lualine').setup({
+        options = {
+          theme = 'gruvbox-material',
+          component_separators = { left = '╱', right = '╱' },
+          section_separators = { left = '', right = '' },
+          globalstatus = true,       -- Одна монолитная строка внизу экрана
+        }
+      })
+
+      -- 5. Настройка парсера Treesitter для глубокой подсветки синтаксиса
+      require('nvim-treesitter.configs').setup({
+        highlight = {
+          enable = true,             -- Включаем Treesitter подсветку
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true }   -- Умные автоотступы на базе синтаксиса
+      })
     '';
   };
 
@@ -58,4 +85,3 @@ inputs: { pkgs, ... }: {
 
   programs.home-manager.enable = true;
 }
-
